d579624fa32fb5a0dfdcf03e0f14f6ae3c80cd58
Srujan P Jain <122275154+Srujan-PJain@users.noreply.github.com>
2023-08-23 07:28:17 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 const menu_nav = document.querySelector("#menucontainer");
 const nav_header = document.querySelector(".navbar");
-
+nav_header.classList.remove("active");
 const toggleNavbar = () => {
   nav_header.classList.toggle("active");
 };
